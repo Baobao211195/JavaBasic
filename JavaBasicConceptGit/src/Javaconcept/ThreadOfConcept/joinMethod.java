86cb066c4f61834f51103f9a68fd7d97b61b4d55
phamvanoanh <oanhmrphamvan@gmail.com>
2015-09-04 10:33:02 +0700

    join method 2

--- JavaBasicConceptGit/src/Javaconcept/ThreadOfConcept/joinMethod.java
+++ JavaBasicConceptGit/src/Javaconcept/ThreadOfConcept/joinMethod.java
@@ -14,38 +14,53 @@ public class joinMethod {
         final Thread t1 = new Thread() {
             public void run() {
                 // thread 1
-                for (int i = 0; i <= 1000; i++) {
+                for (int i = 0; i <= 10; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(joinMethod.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                     System.out.println(i);
                 }
-                System.out.println("Number to from 0 to 1000");
+                System.out.println("Number to from 0 to 10");
             }
         };
         final Thread t2 = new Thread() {
             public void run() {
                 try {
                     // thread 2
-                    t1.join(5000, 500000);
+                    t1.join(11000);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(joinMethod.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                for (int i = 1001; i <= 2000; i++) {
+              for (int i = 11; i <= 20; i++) {  
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(joinMethod.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                     System.out.println(i);
                 }
-                System.out.println("Number from 1001 to 2000");
+                System.out.println("Number from 11 to 20");
             }
         };
         Thread t3 = new Thread() {
             // thread 3
             public void run() {
                 try {
-                    t2.join(5000);
+                    t2.join(21000);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(joinMethod.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                for (int i = 2001; i <= 3000; i++) {
+                for (int i = 21; i <= 30; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(joinMethod.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                     System.out.println(i);
                 }
-                System.out.println("Number from 2001 to 3000");
+                System.out.println("Number from 21 to 30");
             }
         };
 
